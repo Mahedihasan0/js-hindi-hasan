a69fc17d5e97f18c1e00adee824921336f257a8f
Mahedihasan0 <143510829+Mahedihasan0@users.noreply.github.com>
2024-03-25 20:15:34 +0000

    variables confib

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 123123
+let accountName = "Mahedi"
+let accountPassword = "121212"
+let accountEmail = "mh@mh.com"
+let accountCity = "Mumbai"
+
+console.table([accountId, accountName, accountPassword, accountEmail, accountCity])
